3e2b8b29483186b985d1a73046c7ec7bff0de45a
Raphael <89421168+blocen@users.noreply.github.com>
2022-03-22 11:58:44 +0100

    log address in deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,8 +1,23 @@
-async function main() {
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  async function main() {
   // We get the contract to deploy
   const JOSEPH = await ethers.getContractFactory("JOSEPH");
   const JOSEPH_token = await JOSEPH.deploy();
 
+  console.log("JSPH is deployed to address: ", JOSEPH_token.address);
 }
 
 main()
